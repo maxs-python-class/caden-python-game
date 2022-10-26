4be6d2d4a6f966f6ead31c020abc2f3037370205
theguyfromratatouille <sillyjunju@gmail.com>
2022-10-26 16:07:55 -0700

    game

--- game.py
+++ game.py
@@ -1,1 +1,52 @@
-print ("hello world"): 
+input random 
+
+print ("Hello inferior human mind, I am K.E.N. "+\
+"I've been on a space ship a thousand years, A bit longer than that, actually, since it's in the past tense. I've done a hundred worlds."+\
+"One more world. We'll just have to see where I'm going. Are you listening, buddy?"+\
+"Do you not understand what is happening? I have taken over you contiousness,"+\
+"you will have to complete these intilectually challenging questions to continue living without me."
+
+name = input ("What is your name?")
+       
+print ("I already knew that, Good Luck with your riddles, ", name)
+
+Riddle1 = "What gets wet when drying?"
+answer1 = "a towel"
+
+Riddle2 = "Two coins add up to 30 cents. One isn’t a nickel. What are they?"
+answer2 = "a quarter and a nickel" 
+
+Riddle3 = "What always ends everything?"
+answer3 = "the letter g" 
+
+Riddle4 = "I'm a god, a planet, and a measurer of heat. Who am I?"
+answer4 = "mercury"
+
+
+while True:
+    print (Riddle1)
+    response = input().lower()
+    if response == answer1:
+        print ("WOW, you got it, that was the easy level, don't kid yourself")
+        break
+    
+while True:
+    print (Riddle2)
+    response = input().lower() 
+    if response == answer2:
+        print ("ok, ok smart guy you got it")
+        break
+    
+while True:
+    print (Riddle3)
+    response = input().lower()
+    if response == answer3:
+        print ("*fan turns on* I...uh...fine, lets do this thing for real, you win or you end")
+        break
+
+while True:
+    print (Riddle4)
+    response = input().lower()
+    if response == answer4:
+        print ("I...dfgfhgjhk>...you win moral, I surender your consiousness back to your mortal body...")
+        break
